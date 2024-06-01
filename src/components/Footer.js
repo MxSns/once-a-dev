5fd9bcc009658eb75b8b7a67a4724fb584067473
Mxn <oz@pop-os.localdomain>
2024-06-01 16:48:43 +0200

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import '../styles/Footer.css';
 import gitLogo from '../assets/images/github-2.svg';
 import threadsLogo from '../assets/images/threads.svg';
 import twitterLogo from '../assets/images/x-2.svg';
-import ytLogo from '../assets/images/youtube-icon-5.svg';
+import linkdinLogo from '../assets/images/linkdin.svg';
 import mediumLogo from '../assets/images/medium-4.svg';
 
 const Footer = () => {
@@ -16,7 +16,7 @@ const Footer = () => {
               <Link to="https://github.com/MxSns" target="_blank"><img src={gitLogo} alt="Logo GitHub" /></Link>
               <Link to="https://www.threads.net/@mxn.dev" target="_blank"><img src={threadsLogo} alt="Logo GitHub" /></Link>
               <Link to="https://twitter.com/Oz_70th" target="_blank"><img src={twitterLogo} alt="Logo GitHub" /></Link>
-              <Link to="https://github.com/MxSns" target="_blank"><img src={ytLogo} alt="Logo GitHub" /></Link>
+              <Link to="https://www.linkedin.com/in/maxime-sanson-678259310/" target="_blank"><img src={linkdinLogo} alt="Logo GitHub" /></Link>
               <Link to="https://medium.com/@oz_70" target="_blank"><img src={mediumLogo} alt="Logo GitHub" /></Link>
 
 
@@ -24,8 +24,9 @@ const Footer = () => {
         </section>
 
         <footer>
-            <p>Copyright ©OnceUponADev2024.</p>
             <p>Privacy Policy | Terms & Conditions</p>
+            <p>Copyright ©OnceUponADev2024.</p>
+
         </footer>
         </>
     )
